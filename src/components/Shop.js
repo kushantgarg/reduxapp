408f4579c0f77090204f408e88660d555ec67e2a
Kushant Garg <71939372+kushantgarg@users.noreply.github.com>
2022-11-24 13:19:26 +0530

    actiond and reducer creator

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+
+    <div>
+        <h2>Deposit Withdraw Money </h2>
+      <button className="btn btn-primary mx-2">-</button>
+     Update balance 
+      <button className="btn btn-primary mx-2">+</button>
+      
+    </div>
+  )
+}
+
+export default Shop
